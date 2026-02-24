5bde28e7e36e1b2a6a520ca4f71572d91e72a7ad
Mike <mr.mikesorrentino@gmail.com>
2026-02-24 13:55:26 -0500

    Initial Commit

---
+++ tml.2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tml.2.0
+//
+//  Created by mike on 2/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tml.2.0/tml_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  tml_2_0App.swift
+//  tml.2.0
+//
+//  Created by mike on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct tml_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
