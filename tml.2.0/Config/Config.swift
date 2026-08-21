495ed9468a068973594eee4ae26c5fb1800bad0b
Mike <mr.mikesorrentino@gmail.com>
2026-08-21 08:53:13 -0400

    make login to local host for testing

--- tml.2.0/Config/Config.swift
+++ tml.2.0/Config/Config.swift
@@ -1,4 +1,7 @@
 struct Config {
+    #if DEBUG
+    static let baseURL = "http://localhost:8080"
+    #else
     static let baseURL = "https://app-javabackend-5e1ae1d5056c.herokuapp.com"
+    #endif
 }
-
